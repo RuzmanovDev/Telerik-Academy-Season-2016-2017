d3e07ac79a9b4cde5c38c45cc190db5453d532a1
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-04-08 18:04:41 +0300

    Added first 3 Exercises from 6Dec2013

---
+++ C#PartOne/ExamPrep/CSharpPart6OneDecember2013/Enigmanations/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigmanations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            double result = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+                if (currentChar == '(')
+                {
+                    double firstNumber = double.Parse(input[i + 1].ToString());
+
+                    while (currentChar != ')')
+                    {
+                        currentChar = input[i + 1];
+
+                        double seconNumber = double.Parse(input[i + 3].ToString());
+                        char sign = input[i + 2];
+                        switch (sign)
+                        {
+                            case '+': result = firstNumber + seconNumber; break;
+                            case '-': result = firstNumber - seconNumber; break;
+                            case '*': result = firstNumber * seconNumber; break;
+                            case '/': result = firstNumber / seconNumber; break;
+                            case '%': result = firstNumber % seconNumber; break;
+
+                        }
+
+                    }
+
+
+                }
+            }
+        }
+
+    }
+
+}
---
+++ C#PartOne/ExamPrep/CSharpPart6OneDecember2013/Enigmanations/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Enigmanations")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Enigmanations")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9d82273b-6233-428c-86c8-fab5bde0fd34")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
