09ee605b30471c649661b297d16d5574762ba561
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-04-09 10:33:35 +0300

    Added solution for The Carpets.cs problem

--- C#PartOne/ExamPrep/TelerikCSharpPartOne27Dec2012/4.Carpets/Carpets.cs
+++ C#PartOne/ExamPrep/TelerikCSharpPartOne27Dec2012/4.Carpets/Carpets.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 class Carpets
@@ -11,34 +7,123 @@ class Carpets
     {
         int input = int.Parse(Console.ReadLine());
         int topDots = input / 2 - 1;
-
-        for (int i = 0; i < input/2; i++)
+        int currentRol = 2;
+        Console.Write(new string('.', topDots));
+        Console.Write('/');
+        Console.Write('\\');
+        Console.WriteLine(new string('.', topDots));
+        topDots--;
+        int innerLeft = 2;
+        int innerRight = 2;
+        for (int i = 0; i < input / 2 - 1; i++)
         {
-            if (i%2!=0)
+            Console.Write(new string('.', topDots));
+            for (int j = 0; j < innerLeft; j++)
             {
-                Console.Write(new string('.', topDots));
-                Console.Write(" /");
-                Console.Write("\\ ");
-                Console.Write(new string('.', topDots));
-                Console.WriteLine();
-                topDots--;
-
-               
-
-               
+                if (j % 2 == 0)
+                {
+                    Console.Write('/');
+                }
+                else
+                {
+                    Console.Write(' ');
+                }
             }
-            Console.Write(new string('.', topDots));
-            Console.Write("/");
-            Console.Write("\\");
-            Console.Write(new string('.', topDots));
-            Console.WriteLine();
+            if (i % 2 == 0)
+            {
+                for (int k = 0; k < innerRight; k++)
+                {
+                    if (k % 2 == 0)
+                    {
+                        Console.Write(' ');
+                    }
+                    else
+                    {
+                        Console.Write('\\');
+                    }
+                }
+            }
+            else
+            {
+                for (int k = 0; k < innerRight; k++)
+                {
+                    if (k % 2 == 0)
+                    {
+                        Console.Write('\\');
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+            }
+            Console.WriteLine(new string('.', topDots));
             topDots--;
+            innerLeft++;
+            innerRight++;
+            currentRol++;
         }
-        
-       
-        
+
+        //bottom
+        topDots = 0;
+        innerRight--;
+        innerLeft--;
 
 
+        for (int i = 0; i < input / 2 - 1; i++)
+        {
+            Console.Write(new string('.', topDots));
+            for (int j = 0; j < innerLeft; j++)
+            {
+                if (j % 2 == 0)
+                {
+                    Console.Write('\\');
+                }
+                else
+                {
+                    Console.Write(' ');
+                }
+            }
+            if (currentRol % 2 == 0)
+            {
+                for (int k = 0; k < innerRight; k++)
+                {
+                    if (k % 2 == 0)
+                    {
+                        Console.Write('/');
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+            }
+            else
+            {
+                for (int k = 0; k < innerRight; k++)
+                {
+                    if (k % 2 == 0)
+                    {
+                        Console.Write(' ');
+                    }
+                    else
+                    {
+                        Console.Write('/');
+                    }
+                }
+            }
+            Console.WriteLine(new string('.', topDots));
+            topDots++;
+            innerLeft--;
+            innerRight--;
+            currentRol++;
+        }
+
+
+        Console.Write(new string('.', topDots));
+        Console.Write('\\');
+        Console.Write('/');
+        Console.WriteLine(new string('.', topDots));
     }
 }
 
