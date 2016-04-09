41f6929f6d8fb9b88a1cc9936544aa0fb4997180
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-04-09 12:27:55 +0300

    Added solution for Sheets and part of the solution for formula1 bits. Also added tasks description

---
+++ C#PartOne/ExamPrep/TelerikCSharpPartOne27Dec2012/FormulaBit/FormulaBit.cs
@@ -0,0 +1,179 @@
+﻿namespace FormulaBit
+{
+    using System;
+
+    class FormulaBit
+    {
+        static void Main(string[] args)
+        {
+            char[,] matrix = new char[8, 8];
+            string valuesOfMatrix = "";
+            bool cantTurn = false;
+            for (int i = 0; i < 8; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                valuesOfMatrix += Convert.ToString(input, 2).PadLeft(8, '0');
+            }
+
+            //fill the matrix with values 
+            int currentChar = 0;
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    matrix[r, c] = valuesOfMatrix[currentChar];
+                    currentChar++;
+                }
+            }
+
+
+
+            int startRow = 0;
+            int startCol = matrix.GetLength(1) - 1;
+            int lengthOfTheTrack = 0;
+            int numberOfTurns = 0;
+            while (matrix[startRow, startCol] != '1')
+            {
+
+                //move sout row++
+                if (matrix[startRow, startCol] == '0')
+                {
+                    while (matrix[startRow, startCol] != '1')
+                    {
+                        lengthOfTheTrack++;
+                        startRow++;
+                        if (startRow > 7)
+                        {
+                            startRow--;
+                            break;
+                        }
+                    }
+                    if (matrix[startRow, startCol] == '1')
+                    {
+                        startRow--;
+                    }
+
+                    //chech if i am at the end of the track or if i can turn in the next direction  
+                    if (isTheEnd(startRow, startCol) || startCol <= 0) // if it does NOT break the cyle i CAN MOVE in the next direction
+                    {
+                        break;
+                    }
+                    else if (matrix[startRow, startCol - 1] == '1')
+                    {
+                        cantTurn = true;
+                        break;
+                    }
+
+                    //MOVE WEST 
+                    startCol--;
+                    numberOfTurns++;
+                    while (matrix[startRow, startCol] != '1')
+                    {
+                        lengthOfTheTrack++;
+                        startCol--;
+                        if (startCol < 0)
+                        {
+                            startCol++;
+                            break;
+                        }
+                    }
+                    if (matrix[startRow, startCol] == '1')
+                    {
+                        startCol++;
+                    }
+
+                    //chech if i am at the end of the track or if i can turn in the next direction
+                    if (isTheEnd(startRow, startCol) || startRow < 0)
+                    {
+                        break;
+                    }
+                    else if (matrix[startRow - 1, startCol] == '1')
+                    {
+                        cantTurn = true;
+                        break;
+                    }
+
+                    // MOVE NORTH
+                    startRow--;
+                    numberOfTurns++;
+
+                    while (matrix[startRow, startCol] != '1')
+                    {
+                        lengthOfTheTrack++;
+                        startRow--;
+                        if (startRow < 0)
+                        {
+                            startRow++;
+                            break;
+                        }
+                    }
+                    if (matrix[startRow, startCol] == '1')
+                    {
+                        startRow--;
+                    }
+
+                    //chech if i am at the end of the track or if i can turn in the next direction  ???????????
+                    if (isTheEnd(startRow, startCol) || startCol <= 0)
+                    {
+                        break;
+                    }
+                    else if (matrix[startRow, startCol - 1] == '1')
+                    {
+                        cantTurn = true;
+                        break;
+                    }
+                    //MOVE WEST
+                    startCol--;
+                    numberOfTurns++;
+
+                    while (matrix[startRow, startCol] != '1')
+                    {
+                        lengthOfTheTrack++;
+                        startCol--;
+                        if (startCol < 0)
+                        {
+                            startCol++;
+                            break;
+                        }
+                    }
+                    if (matrix[startRow, startCol] == '1')
+                    {
+                        startCol++;
+                    }
+                    if (isTheEnd(startRow, startCol) || startRow > 7) // if it does NOT break the cyle i CAN MOVE in the next direction
+                    {
+                        break;
+                    }
+                    else if (matrix[startRow + 1, startCol] == '1')
+                    {
+                        cantTurn = true;
+                        break;
+                    }
+                    startRow++;
+                    numberOfTurns++;
+                }
+
+            }
+            if (cantTurn)
+            {
+                Console.WriteLine("No " + lengthOfTheTrack);
+            }
+            else
+            {
+                Console.WriteLine(lengthOfTheTrack + " " + numberOfTurns);
+
+            }
+
+
+        }
+
+        static bool isTheEnd(int startRow, int startCol)
+        {
+            if (startRow == 7 && startCol == 7)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ C#PartOne/ExamPrep/TelerikCSharpPartOne27Dec2012/Sheets/Sheets.cs
@@ -0,0 +1,25 @@
+﻿namespace Sheets
+{
+    using System;
+
+    class Sheets
+    {
+        static void Main(string[] args)
+        {
+            int[] sheets = { 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1 };
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < sheets.Length; i++)
+            {
+                if (sheets[i] > number)
+                {
+                    Console.WriteLine("A{0}", i);
+                }
+                else
+                {
+                    number -= sheets[i];
+                }
+                
+            }
+        }
+    }
+}
