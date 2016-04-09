04f3c52d0c1eb0f49294cd601b07c8450ddf1814
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-04-09 21:29:03 +0300

    Testing stylecop

--- C#PartOne/ExamPrep/TelerikCSharpPartOne27Dec2012/1.NextDate/Program.cs
+++ C#PartOne/ExamPrep/TelerikCSharpPartOne27Dec2012/1.NextDate/Program.cs
@@ -1,26 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _1.NextDate
+﻿namespace _1.NextDate
 {
-    class Program
+    using System;
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             int day = int.Parse(Console.ReadLine());
             int month = int.Parse(Console.ReadLine());
             int year = int.Parse(Console.ReadLine());
 
-
             DateTime date = new DateTime(day, month, year);
             DateTime nextDate = date.AddDays(1);
 
             Console.WriteLine(nextDate.Day + "." + nextDate.Month + "." + nextDate.Year);
-
-
         }
     }
 }
