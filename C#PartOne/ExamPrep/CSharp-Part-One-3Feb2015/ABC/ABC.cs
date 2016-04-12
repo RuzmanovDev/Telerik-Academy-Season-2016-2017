c793f2c42f02473b26d8600e83b55f428f2d1397
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-04-13 01:20:21 +0300

    Added First exercises from CSharp-Part-One-3Feb2015

---
+++ C#PartOne/ExamPrep/CSharp-Part-One-3Feb2015/ABC/ABC.cs
@@ -0,0 +1,24 @@
+﻿namespace ABC
+{
+    using System;
+
+    class ABC
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int biggest = Math.Max(Math.Max(a, b), c);
+            int min = Math.Min(Math.Min(a, b), c);
+
+            int sum = a + b + c;
+            double aritmetical = sum / 3d;
+
+            Console.WriteLine(biggest);
+            Console.WriteLine(min);
+            Console.WriteLine("{0:F3}", aritmetical);
+        }
+    }
+}
---
+++ C#PartOne/ExamPrep/CSharp-Part-One-3Feb2015/SymbolToNumbers/SymbolToNumbers.cs
@@ -0,0 +1,48 @@
+﻿namespace SymbolToNumbers
+{
+    using System;
+
+    class SymbolToNumbers
+    {
+        static void Main(string[] args)
+        {
+            int secretNumber = int.Parse(Console.ReadLine());
+            string text = Console.ReadLine();
+            double result = 0;
+            double secondResult = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentChar = text[i];
+                if (currentChar=='@')
+                {
+                    break;
+                }
+                else if (Char.IsLetter(currentChar))
+                { 
+                    result = (int)currentChar * secretNumber + 1000;
+                }
+                else if (Char.IsDigit(currentChar))
+                {
+                    result = currentChar + secretNumber + 500;
+                }
+                else
+                {
+                    result = currentChar - secretNumber;
+                }
+                if (i%2==0)
+                {
+                    secondResult = result / 100d;
+                    Console.WriteLine("{0:F2}", secondResult);
+                }
+                else
+                {
+                    secondResult = result * 100;
+                    Console.WriteLine("{0}", (int)secondResult);
+                 
+                }
+                
+            }
+        }
+    }
+}
