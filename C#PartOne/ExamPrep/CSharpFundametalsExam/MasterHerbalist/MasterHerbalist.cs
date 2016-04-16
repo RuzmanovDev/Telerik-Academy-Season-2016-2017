ec1dd4bbfc19be08f46a2113d08c188a285f35bd
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-04-16 11:27:40 +0300

    Added MasterHerbalist.cs Removed Wrong A NACCI

---
+++ C#PartOne/ExamPrep/CSharpFundametalsExam/MasterHerbalist/MasterHerbalist.cs
@@ -0,0 +1,69 @@
+﻿namespace MasterHerbalist
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Threading;
+
+    public class MasterHerbalist
+    {
+        public static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            int dailyExpanses = int.Parse(Console.ReadLine());
+            List<int> totalMoneyPerDay = new List<int>();
+            int days = 0;
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                if (input[0] == "Season")
+                {
+                    break;
+                }
+
+                int hours = int.Parse(input[0]);
+                string path = input[1];
+                int price = int.Parse(input[2]);
+                days++;
+
+                int herbsGathered = 0;
+
+                do
+                {
+                    for (int i = 0; i < path.Length; i++)
+                    {
+                        if (path[i] == 'H')
+                        {
+                            herbsGathered++;
+                        }
+
+                        hours--;
+                        if (hours == 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+                while (hours > 0);
+
+                int moneyGained = herbsGathered * price;
+                totalMoneyPerDay.Add(moneyGained);
+            }
+
+            long totalMoney = totalMoneyPerDay.Sum();
+            double average = (double)totalMoney / days;
+
+            if (average >= dailyExpanses)
+            {
+                Console.WriteLine("Times are good. Extra money per day: {0:F2}.", average - dailyExpanses);
+            }
+            else
+            {
+                Console.WriteLine("We are in the red. Money needed: {0}.", (int)((dailyExpanses * days) - totalMoney));
+            }
+        }
+    }
+}
--- C#PartOne/ExamPrep/CSharpFundametalsExam/MasterHerbalist/Properties/AssemblyInfo.cs
+++ C#PartOne/ExamPrep/CSharpFundametalsExam/MasterHerbalist/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ANacci")]
+[assembly: AssemblyTitle("MasterHerbalist")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ANacci")]
+[assembly: AssemblyProduct("MasterHerbalist")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b163cb45-06b1-467a-a76d-6f6e28106778")]
+[assembly: Guid("d5d623b5-e6d0-4ad0-9106-d28498214aeb")]
 
 // Version information for an assembly consists of the following four values:
 //
