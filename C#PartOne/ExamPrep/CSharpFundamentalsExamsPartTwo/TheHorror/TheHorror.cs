6cf318945d2a91c474a414924489316f3e544cea
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-04-18 17:07:28 +0300

    Added TheHorror.cs

---
+++ C#PartOne/ExamPrep/CSharpFundamentalsExamsPartTwo/TheHorror/TheHorror.cs
@@ -0,0 +1,25 @@
+﻿namespace TheHorror
+{
+    using System;
+
+    public class TheHorror
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            long evenCount = 0;
+            long evenSum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i % 2 == 0 && char.IsDigit(input[i]))
+                {
+                    evenCount++;
+                    evenSum += input[i] - '0';
+                }
+            }
+
+            Console.WriteLine("{0} {1}", evenCount, evenSum);
+        }
+    }
+}
