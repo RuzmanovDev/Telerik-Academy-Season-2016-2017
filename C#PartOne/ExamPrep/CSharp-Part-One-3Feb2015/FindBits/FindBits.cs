0983f2f83448e395e2fcc8eb188a0bf3fbcad347
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-04-18 11:21:19 +0300

    Added Soluton for FindBits

--- C#PartOne/ExamPrep/CSharp-Part-One-3Feb2015/FindBits/FindBits.cs
+++ C#PartOne/ExamPrep/CSharp-Part-One-3Feb2015/FindBits/FindBits.cs
@@ -1,15 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FindBits
+﻿namespace FindBits
 {
-    class Program
+    using System;
+
+    class FindBits
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            int input = int.Parse(Console.ReadLine());
+            string pattern = Convert.ToString(input, 2).PadLeft(5, '0');
+            int bitsCount = 0;
+
+            int times = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < times; i++)
+            {
+                string currentNumber = Convert.ToString(long.Parse(Console.ReadLine()), 2).PadLeft(29, '0');
+                for (int index = 0; index < currentNumber.Length - 4; index++)
+                {
+                    int hasPattern = 0;
+                    string checkingPattern = currentNumber.Substring(index, 5);
+                    for (int j = 0; j < pattern.Length; j++)
+                    {
+                        if (pattern[j] == checkingPattern[j])
+                        {
+                            hasPattern++;
+                        }
+                        else
+                        {
+                            hasPattern = 0;
+                        }
+
+                        if (hasPattern == 5)
+                        {
+                            bitsCount++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(bitsCount);
         }
     }
 }
