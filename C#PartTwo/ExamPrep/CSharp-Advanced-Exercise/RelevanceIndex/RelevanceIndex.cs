9e99832477daf55821d18b6cb8234247138344b1
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-04-26 15:44:29 +0300

    Added solution for RelevanceIndex

---
+++ C#PartTwo/ExamPrep/CSharp-Advanced-Exercise/RelevanceIndex/RelevanceIndex.cs
@@ -0,0 +1,57 @@
+﻿namespace RelevanceIndex
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class RelevanceIndex
+    {
+        static void Main(string[] args)
+        {
+            string search = Console.ReadLine();
+            int numberOfParagraphs = int.Parse(Console.ReadLine());
+            int searchWordCount = 0;
+            SortedDictionary<int, string> result = new SortedDictionary<int, string>();
+
+            for (int i = 0; i < numberOfParagraphs; i++)
+            {
+                string[] currentParagraph = Console.ReadLine().Split(new char[] { ',', '.', '(', ')', ';', '-', '!', '?', ' '}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                searchWordCount = 0;
+                StringBuilder formatedParagraph = new StringBuilder();
+                for (int j = 0; j < currentParagraph.Length; j++)
+                {
+                    string currentWord = currentParagraph[j];
+                    if (currentWord.ToLower() == search.ToLower())
+                    {
+                        searchWordCount++;
+                        currentWord = currentWord.ToUpper();
+                        formatedParagraph.Append(currentWord + " ");
+                    }
+                    else
+                    {
+                        formatedParagraph.Append(currentWord + " ");
+                    }
+                }
+                try
+                {
+                    result.Add(searchWordCount, formatedParagraph.ToString());
+                }
+                catch (Exception)
+                {
+                    searchWordCount++;
+                    result.Add(searchWordCount, formatedParagraph.ToString());
+
+                }
+               
+                //formatedParagraph.Clear();
+
+            }
+            
+            foreach (string paragraph in result.Values.Reverse())
+            {
+                Console.WriteLine("{0}", paragraph);
+            }
+        }
+    }
+}
