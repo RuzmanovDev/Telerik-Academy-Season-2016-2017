d1c7732223e2995e48a096d3fa63259debdbbd53
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-05-08 16:56:35 +0300

    Added StrangelandNumbers.cs

---
+++ C#PartTwo/ExamPrep/CSharp-Advanced-Exercise/StrangelandNumbers/StrangelandNumbers.cs
@@ -0,0 +1,38 @@
+﻿namespace StrangelandNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class StrangelandNumbers
+    {
+        static void Main(string[] args)
+        {
+            string[] alphabet = { "f", "bIN", "oBJEC", "mNTRAVL", "lPVKNQ", "pNWE", "hT" };
+            string input = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+            List<int> decimalValues = new List<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string currentWord = input[i].ToString();
+                sb.Append(currentWord);
+
+                if (alphabet.Contains(sb.ToString()))
+                {
+                    decimalValues.Add(Array.IndexOf(alphabet, sb.ToString()));
+                    sb.Clear();
+                }
+            }
+            long result = 0;
+
+            for (int i = 0; i < decimalValues.Count; i++)
+            {
+                result *= 7;
+                result += decimalValues[i];
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
