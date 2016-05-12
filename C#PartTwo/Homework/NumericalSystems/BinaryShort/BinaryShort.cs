2008ae61ceb4684b44e8797c53dab940bccc7415
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-05-13 00:26:03 +0300

    Add the rest of the HW for NumericalSystems

---
+++ C#PartTwo/Homework/NumericalSystems/BinaryShort/BinaryShort.cs
@@ -0,0 +1,33 @@
+﻿namespace BinaryShort
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class BinaryShort
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            string binary = ShortToBinaryConvertion(number);
+            // Console.WriteLine(Convert.ToString(number, 2).PadLeft(16, '0'));
+        
+            Console.WriteLine(binary);
+
+        }
+
+        private static string ShortToBinaryConvertion(int number)
+        {
+            string binaryNumber = string.Empty;
+
+            for (int i = 15; i >= 0; i--)
+            {
+                binaryNumber = ((number % 2) & 1) + binaryNumber;
+                number >>= 1;
+                //if (i % 4 == 0) binary = " " + binary;    // for better print
+            }
+            return binaryNumber;
+        }
+    }
+}
---
+++ C#PartTwo/Homework/NumericalSystems/OneSystemToAnyOther/OneSystemToAnyOther.cs
@@ -0,0 +1,72 @@
+﻿namespace OneSystemToAnyOther
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class OneSystemToAnyOther
+    {
+        static char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+        public static void Main(string[] args)
+        {
+            int systemBase = int.Parse(Console.ReadLine());
+            string number = Console.ReadLine();
+
+            int toBase = int.Parse(Console.ReadLine());
+
+            string result = UniversalConverter(number, systemBase, toBase);
+            Console.WriteLine(result);
+
+        }
+
+        private static string UniversalConverter(string number, int systemBase, int toBase)
+        {
+            long decimalNumber = 0;
+            // first convert it to decimal
+            if (systemBase == 10)
+            {
+                decimalNumber = long.Parse(number);
+            }
+            else
+            {
+                decimalNumber = ConvertToDecimal(number, systemBase);
+            }
+
+            // Then convert it to the specified system
+            var residuals = new Stack<char>();
+
+            while (decimalNumber != 0)
+            {
+                long residual = decimalNumber % toBase;
+                char hexChar = digits[residual];
+
+                residuals.Push(hexChar);
+
+                decimalNumber /= toBase;
+            }
+
+            var result = new StringBuilder();
+
+            while (residuals.Count != 0)
+            {
+                result.Append(residuals.Pop());
+            }
+
+            return result.ToString();
+        }
+
+        private static long ConvertToDecimal(string number, int systemBase)
+        {
+            long decimalNumber = 0;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                decimalNumber *= systemBase;
+                int weight = Array.IndexOf(digits, number[i]);
+                decimalNumber += weight;
+            }
+            return decimalNumber;
+        }
+    }
+}
