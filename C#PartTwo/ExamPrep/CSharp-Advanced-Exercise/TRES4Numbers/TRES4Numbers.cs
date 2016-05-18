348e95421341ac10bedc5b1c4a7a986390753781
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-05-18 19:05:52 +0300

    Added TRES4Numbers

---
+++ C#PartTwo/ExamPrep/CSharp-Advanced-Exercise/TRES4Numbers/TRES4Numbers.cs
@@ -0,0 +1,39 @@
+﻿namespace TRES4Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class TRES4Numbers
+    {
+        static void Main(string[] args)
+        {
+            string[] digits = { "LON+", "VK-", "*ACAD", "^MIM", "ERIK|", "SEY&", "EMY>>", "/TEL", "<<DON" };
+            ulong number = ulong.Parse(Console.ReadLine());
+            if (number == 0)
+            {
+                Console.WriteLine(digits[0]);
+                return;
+            }
+
+            List<ulong> residuals = new List<ulong>();
+            StringBuilder result = new StringBuilder();
+            while (number != 0)
+            {
+                ulong residual = number % 9;
+                residuals.Add(residual);
+
+                number /= 9;
+            }
+
+            for (int i = residuals.Count - 1; i >= 0; i--)
+            {
+                int currentDigitIndex = (int)residuals[i];
+                string currentDgit = digits[currentDigitIndex];
+                result.Append(currentDgit);
+            }
+           
+                Console.WriteLine(result.ToString());
+        }
+    }
+}
