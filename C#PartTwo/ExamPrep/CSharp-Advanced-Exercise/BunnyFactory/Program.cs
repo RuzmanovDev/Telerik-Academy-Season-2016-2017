10fb316986f478f8c0763df7bd442e0d7b57621d
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-05-19 16:08:33 +0300

    Added Solution for XPatterns (patterns)

---
+++ C#PartTwo/ExamPrep/CSharp-Advanced-Exercise/BunnyFactory/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunnyFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            List<BigInteger> cages = new List<BigInteger>();
+            cages.Add(N);
+            int cycles = 1;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "END")
+                {
+                    break;
+                }
+                BigInteger num = BigInteger.Parse(command);
+                cages.Add(num);
+            }
+
+            // multiplication proccess
+            while (true)
+            {
+                if (cycles > cages.Count)
+                {
+                    break;
+                }
+                BigInteger sum = 0;
+                int i = 0;
+
+                while (i < cycles) // sum item depending on the cycle 
+                {
+                    sum += cages[i];
+                    i++;
+                }
+
+                BigInteger nextSum = 0;
+                BigInteger product = 1;
+
+                // TODO: 40/100
+
+                BigInteger multiplicationLoop = i + sum;
+                if (multiplicationLoop > cages.Count - 1)
+                {
+                    break;
+                }
+                while (i < multiplicationLoop)
+                {
+                    nextSum += cages[i];
+                    product *= cages[i];
+                    i++;
+                }
+
+                // remove elements and concatinate other
+                i--;
+
+                cages.RemoveRange(0, cages.Count - (cages.Count - 1 - i));
+                // appends result
+                while (product != 0)
+                {
+                    BigInteger currentDigit = product % 10;
+                    cages.Insert(0, currentDigit);
+                    product /= 10;
+                }
+                // appends result
+                while (nextSum != 0)
+                {
+                    BigInteger currentDigit = nextSum % 10;
+                    cages.Insert(0, currentDigit);
+                    nextSum /= 10;
+                }
+
+                // remove 0 and 1 
+                cages.RemoveAll(n => n == 0 || n == 1);
+                // end of the first cycle
+                cycles++;
+            }
+
+            Console.WriteLine(string.Join(" ", cages));
+        }
+    }
+}
---
+++ C#PartTwo/ExamPrep/CSharp-Advanced-Exercise/XPatterns/XPatterns.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XPatterns
+{
+    class XPatterns
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long[,] matrix = new long[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 0; j < line.Length; j++)
+                {
+                    matrix[i, j] = long.Parse(line[j]);
+                }
+            }
+
+            long maxSum = long.MinValue;
+            bool found = false;
+            for (int row = 0; row <= matrix.GetLength(0) - 2; row++)
+            {
+                long sum = 0;
+                for (int col = 0; col <= matrix.GetLength(1) - 4; col++)
+                {
+                    if (matrix[row, col] + 1 == matrix[row, col + 1] && matrix[row, col] + 2 == matrix[row, col + 2]
+                        && matrix[row, col + 2] + 1 == matrix[row + 1, col + 2] && matrix[row, col + 2] + 2 == matrix[row + 2, col + 2]
+                        && matrix[row + 2, col + 2] + 1 == matrix[row + 2, col + 3] && matrix[row + 2, col + 2] + 2 == matrix[row + 2, col + 4])
+                    {
+                        found = true;
+                        sum += matrix[row, col];
+                        sum += matrix[row, col + 1];
+                        sum += matrix[row, col + 2];
+                        sum += matrix[row + 1, col + 2];
+                        sum += matrix[row + 2, col + 2];
+                        sum += matrix[row + 2, col + 3];
+                        sum += matrix[row + 2, col + 4];
+                    }
+                    maxSum = Math.Max(sum, maxSum);
+                }
+            }
+            if (!found)
+            {
+                long sumOfMain = SumOfMainDiagonal(matrix);
+
+                Console.WriteLine("NO {0}", sumOfMain);
+            }
+            else
+            {
+                Console.WriteLine("YES {0}", maxSum);
+
+            }
+
+        }
+        static long SumOfMainDiagonal(long[,] matrix)
+        {
+            long sum = 0;
+            int r = 0;
+            int c = 0;
+
+            while (r < matrix.GetLength(1))
+            {
+                sum += matrix[r, c];
+                r++;
+                c++;
+            }
+            return sum;
+        }
+    }
+}
