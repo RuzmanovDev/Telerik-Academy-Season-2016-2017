663b5aa95771f2e745d103d39f2001aad316048d
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-05-28 13:03:23 +0300

    Added Exceptions-Handling HW for BGcoder

---
+++ C#PartTwo/Homework/Exceptions-Handling/ReadNumbers/ReadNumbers.cs
@@ -0,0 +1,37 @@
+﻿namespace ReadNumbers
+{
+    using System;
+
+    class ReadNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            try
+            {
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    if (numbers[i] <= numbers[i - 1] || numbers[i] <= 1 || numbers[i] >= 100)
+                    {
+                        throw new Exception();
+                    }
+                }
+
+                Console.Write("1 < ");
+                Console.Write(string.Join(" < ", numbers));
+                Console.WriteLine(" < 100");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+
+            }
+        }
+    }
+}
---
+++ C#PartTwo/Homework/Exceptions-Handling/SquareRoot/SquareRoot.cs
@@ -0,0 +1,35 @@
+﻿namespace SquareRoot
+{
+    using System;
+    using System.Globalization;
+    using System.Threading;
+
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            try
+            {
+                double n = double.Parse(Console.ReadLine());
+                double sqrt = Math.Sqrt(n);
+                if (n < 0)
+                {
+                    throw new Exception();
+                }
+
+                Console.WriteLine("{0:F3}", sqrt);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+
+        }
+    }
+}
