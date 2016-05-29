bdfcb866af9a667e9d872725d5ef0cd0ca041e3b
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-05-29 13:42:51 +0300

    Add JoroTheRabbit.cs and DurankolakNumbers.cs

---
+++ C#PartTwo/ExamPrep/CSharp-Advanced-Exercise/DurankolakNumbers/DurankolakNumbers.cs
@@ -0,0 +1,77 @@
+﻿namespace DurankolakNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class DurankolakNumbers
+    {
+        static void Main(string[] args)
+        {
+            char smallLetter = 'a';
+            char capitalLetter = 'A';
+
+            string[] digits = new string[168];
+            for (int i = 0; i < 26; i++)
+            {
+                digits[i] = capitalLetter.ToString();
+                capitalLetter++;
+                capitalLetter = capitalLetter > 'Z' ? 'A' : capitalLetter;
+
+            }
+            for (int i = 26; i < digits.Length; i++)
+            {
+                if (i >= 52)
+                {
+                    smallLetter = 'b';
+                }
+                if (i >= 78)
+                {
+                    smallLetter = 'c';
+                }
+                if (i >= 104)
+                {
+                    smallLetter = 'd';
+                }
+                if (i >= 130)
+                {
+                    smallLetter = 'd';
+                }
+                if (i >= 156)
+                {
+                    smallLetter = 'f';
+                }
+                string currentDigit = smallLetter.ToString() + capitalLetter.ToString();
+                capitalLetter++;
+                capitalLetter = capitalLetter > 'Z' ? 'A' : capitalLetter;
+
+                digits[i] = currentDigit;
+            }
+
+            string input = Console.ReadLine();
+            BigInteger result = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string currentDigit = string.Empty;
+                if (char.IsUpper(input[i]))
+                {
+                    currentDigit = input[i].ToString();
+                }
+                else
+                {
+                    currentDigit = input.Substring(i, 2);
+                    i += 1;
+                }
+                BigInteger value = (uint)Array.IndexOf(digits, currentDigit);
+                result *= 168;
+                result += value;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ C#PartTwo/ExamPrep/CSharp-Advanced-Exercise/JoroTheRabbit/JoroTheRabbit.cs
@@ -0,0 +1,53 @@
+﻿namespace JoroTheRabbit
+{
+    using System;
+    using System.Linq;
+
+    class JoroTheRabbit
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int maxLengh = 0;
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    // bool[] visited = new bool[numbers.Length]; this is useless 75/100 slow
+                    int currentPosition = j;
+                    int step = i;
+                    int length = 0;
+                    while (true)
+                    {
+                        // visited[currentPosition] = true;
+                        length++;
+                        int nextPosition = 0;
+                        if (currentPosition + step >= numbers.Length)
+                        {
+                            nextPosition = (currentPosition + step) - numbers.Length;
+                        }
+                        else
+                        {
+                            nextPosition = currentPosition + step;
+                        }
+                        //int nextPosition = (currentPosition + step) % numbers.Length; // with this 95 /100
+
+                        if (numbers[currentPosition] < numbers[nextPosition]) //&& !visited[nextPosition])
+                        {
+                            currentPosition = nextPosition;
+                        }
+                        else
+                        {
+                            maxLengh = Math.Max(length, maxLengh);
+                            break;
+                        }
+                    }
+
+                }
+            }
+            Console.WriteLine(maxLengh);
+
+        }
+    }
+}
