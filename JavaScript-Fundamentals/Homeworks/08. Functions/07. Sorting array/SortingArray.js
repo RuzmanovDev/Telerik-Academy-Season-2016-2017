e1ff189cd67ba74b6359673552d5a2cf2260b118
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-06-27 17:42:07 +0300

    Fixed SortingArray.js

--- JavaScript-Fundamentals/Homeworks/08. Functions/07. Sorting array/SortingArray.js
+++ JavaScript-Fundamentals/Homeworks/08. Functions/07. Sorting array/SortingArray.js
@@ -3,6 +3,11 @@ function solve(args) {
         i,
         length;
 
-    
-    return numbers.sort().join(' ');
+    return numbers.sort(function (x, y) {
+        if (x > y) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }).join(' ');
 }
