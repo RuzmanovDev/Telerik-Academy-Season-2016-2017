f15c779a117a028f00b8e41ac95b4da06f88ca7e
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-08-27 13:21:12 +0300

    Added HW Code-Formating

---
+++ Modul-II/01.High-Quality-Code/02.HQC-Part-One/Homeworks/02.Code-Formating/Events/EntryPoint.cs
@@ -0,0 +1,19 @@
+﻿namespace Events
+{
+    using Contracts;
+
+    public class EntryPoint
+    {
+        public static void Main()
+        {
+            ICommandReader consoleReader = new ConsoleReader();
+            IEventsHolder eventsHolder = new EventsHolder();
+
+            var commandExecutor = new CommandExecutor(eventsHolder, consoleReader);
+
+            while (commandExecutor.ExecuteNextCommand())
+            {
+            }
+        }
+    }
+}
---
+++ Modul-II/01.High-Quality-Code/02.HQC-Part-One/Homeworks/02.Code-Formating/Events/EventsHolder.cs
@@ -0,0 +1,63 @@
+﻿namespace Events
+{
+    using System;
+
+    using Contracts;
+    using Wintellect.PowerCollections;
+
+    public class EventsHolder : IEventsHolder
+    {
+        private MultiDictionary<string, Event> eventsByTitle;
+        private OrderedBag<Event> eventsByDate;
+
+        public EventsHolder()
+        {
+            this.eventsByTitle = new MultiDictionary<string, Event>(true);
+            this.eventsByDate = new OrderedBag<Event>();
+        }
+
+        public void AddEvent(DateTime date, string title, string location)
+        {
+            Event newEvent = new Event(date, title, location);
+            this.eventsByTitle.Add(title.ToLower(), newEvent);
+            this.eventsByDate.Add(newEvent);
+            Messages.EventAdded();
+        }
+
+        public void DeleteEvents(string titleToDelete)
+        {
+            string title = titleToDelete.ToLower();
+            int removed = 0;
+            foreach (var eventToRemove in this.eventsByTitle[title])
+            {
+                removed++;
+                this.eventsByDate.Remove(eventToRemove);
+            }
+
+            this.eventsByTitle.Remove(title);
+            Messages.EventDeleted(removed);
+        }
+
+        public void ListEvents(DateTime date, int count)
+        {
+            OrderedBag<Event>.View eventsToShow = this.eventsByDate.RangeFrom(new Event(date, string.Empty, string.Empty), true);
+            int showed = 0;
+            foreach (var eventToShow in eventsToShow)
+            {
+                if (showed == count)
+                {
+                    break;
+                }
+
+                Messages.PrintEvent(eventToShow);
+
+                showed++;
+            }
+
+            if (showed == 0)
+            {
+                Messages.NoEventsFound();
+            }
+        }
+    }
+}
