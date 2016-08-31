ff0793c4da6a1145f61da1675a060cfef6faeed5
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-08-31 19:00:30 +0300

    Added homework for Variables-Data-Expressions-and-Constants

---
+++ Modul-II/01.High-Quality-Code/02.HQC-Part-One/Homeworks/05.Variables-Data-Expressions-and-Constants/Task1/Size.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Size
+    {
+        private double width;
+        private double height;
+
+        public Size(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public static Size GetRotatedSize(
+            Size size, double angle)
+        {
+            var sizeWidth = CalculateAbsoluteWidth(size, angle);
+            var sizeHeigth = CalculateAbsoluteHeight(size, angle);
+            var result = new Size(sizeWidth, sizeHeigth);
+
+            return result;
+        }
+
+        private static double CalculateAbsoluteWidth(Size size, double angle)
+        {
+            double abosuluteWidth = (Math.Abs(Math.Cos(angle)) * size.width) + (Math.Abs(Math.Sin(angle)) * size.height);
+
+            return abosuluteWidth;
+        }
+
+        private static double CalculateAbsoluteHeight(Size size, double angle)
+        {
+            double absoluteHeigth = (Math.Abs(Math.Sin(angle)) * size.width) + (Math.Abs(Math.Cos(angle)) * size.height);
+
+            return absoluteHeigth;
+        }
+    }
+}
---
+++ Modul-II/01.High-Quality-Code/02.HQC-Part-One/Homeworks/05.Variables-Data-Expressions-and-Constants/Task2/Printer.cs
@@ -0,0 +1,56 @@
+﻿namespace Task2
+{
+    public class Printer
+    {
+        public void PrintStatistics(double[] numbers, int numbersCount)
+        {
+            // First calculate the values -> better design will be if they are passed as method parameters
+            this.CalculateMinimumvalue(numbers, numbersCount);
+            this.CalculateMaximumValue(numbers, numbersCount);
+            this.CalculateAverageValue(numbers, numbersCount);
+
+            // Some kind of printing logic
+        }
+
+        private void CalculateMinimumvalue(double[] numbers, int numbersCount)
+        {
+            double minimum = numbers[0];
+
+            for (int i = 0; i < numbersCount; i++)
+            {
+                double number = numbers[i];
+                if (number < minimum)
+                {
+                    minimum = number;
+                }
+            }
+        }
+
+        private void CalculateMaximumValue(double[] numbers, int numbersCount)
+        {
+            double maximum = numbers[0];
+
+            for (int i = 0; i < numbersCount; i++)
+            {
+                double number = numbers[i];
+                if (number > maximum)
+                {
+                    maximum = number;
+                }
+            }
+        }
+
+        private void CalculateAverageValue(double[] numbers, int numbersCount)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < numbersCount; i++)
+            {
+                double number = numbers[i];
+                sum += number;
+            }
+
+            double average = sum / numbersCount;
+        }
+    }
+}
