b74d0c9cdccc12e3d21acdd47771c6ac04798ec1
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-09-17 12:08:20 +0300

    Added hw for code optimization and tuning

---
+++ Modul-II/01.High-Quality-Code/03.HQC-Part-Two/Homeworks/02.Code-Tuning-and-Optimization/Compare-Advanced-Maths/AdvancedMathTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Compare_Advanced_Maths
+{
+    public class AdvancedMathTest
+    {
+        private const int CycleCount = 1000000;
+        private const int NumberToDoOperationWith = 1;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Double");
+            TestIngSimpleMath(2D);
+
+            Console.WriteLine("Decimal");
+            TestIngSimpleMath(2M);
+
+            Console.WriteLine("Float");
+            TestIngSimpleMath(2F);
+        }
+
+        private static void TestIngSimpleMath(dynamic number)
+        {
+            Console.WriteLine("Square root");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < CycleCount; i++)
+                {
+                    Math.Sqrt((double)number);
+                }
+            });
+
+            Console.WriteLine("Natural Logarithm");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < CycleCount; i++)
+                {
+                    Math.Log((double)number);
+                }
+            });
+
+            Console.WriteLine("Sinus");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < CycleCount; i++)
+                {
+                    Math.Sin((double)number);
+                }
+            });
+
+            PrintSeperator();
+        }
+
+        private static void PrintSeperator()
+        {
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine();
+        }
+
+        private static void DisplayExecutionTime(Action action)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+    }
+}
---
+++ Modul-II/01.High-Quality-Code/03.HQC-Part-Two/Homeworks/02.Code-Tuning-and-Optimization/CompareSimpleMath/SimpleMathTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CompareSimpleMath
+{
+    public class SimpleMathTest
+    {
+        private const int CycleCount = 1000000;
+        private const int NumberToDoOperationWith = 1;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Int");
+            TestIngSimpleMath(2);
+
+            Console.WriteLine("Long");
+            TestIngSimpleMath(2F);
+
+            Console.WriteLine("Double");
+            TestIngSimpleMath(2D);
+
+            Console.WriteLine("Decimal");
+            TestIngSimpleMath(2M);
+
+            Console.WriteLine("Float");
+            TestIngSimpleMath(2F);
+        }
+
+        private static void TestIngSimpleMath(dynamic number)
+        {
+            Console.WriteLine("Addition");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < CycleCount; i++)
+                {
+                    number += NumberToDoOperationWith;
+                }
+            });
+
+            Console.WriteLine("Substraction");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < CycleCount; i++)
+                {
+                    number -= NumberToDoOperationWith;
+                }
+            });
+
+            Console.WriteLine("Multiplication");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < CycleCount; i++)
+                {
+                    number *= NumberToDoOperationWith;
+                }
+            });
+
+            Console.WriteLine("Devision");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 1; i < CycleCount; i++)
+                {
+                    number /= NumberToDoOperationWith;
+                }
+            });
+
+            Console.WriteLine("Increment");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 1; i < CycleCount; i++)
+                {
+                    number++;
+                }
+            });
+
+            PrintSeperator();
+        }
+
+        private static void PrintSeperator()
+        {
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine();
+        }
+
+        private static void DisplayExecutionTime(Action action)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+    }
+}
