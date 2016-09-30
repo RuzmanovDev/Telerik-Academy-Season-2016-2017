27f92f343631dd16500756afbcb2f9b2815cbb9c
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-10-01 00:47:59 +0300

    Added hw for development tools

---
+++ Modul-II/01.High-Quality-Code/03.HQC-Part-Two/Homeworks/05.Development-Tools/Log4Net/Log4NetExample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+namespace Log4Net
+{
+    public class Log4NetExample
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Log4NetExample));
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+
+            int[] numbers = new int[20];
+            Log.Info("Info logging");
+            try
+            {
+                for (int i = 0; i <= numbers.Length; i++)
+                {
+                    numbers[i] = i;
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Log.Error("We went outside of the array", e);
+            }
+        }
+    }
+}
